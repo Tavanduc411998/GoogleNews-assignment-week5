232b3c9537ca467d3aadc0f6fa8334731fd90920
Tavanduc411998 <57761776+Tavanduc411998@users.noreply.github.com>
2020-10-11 23:04:03 +0700

    assignment-week5

--- App.js
+++ App.js
@@ -1,21 +1,161 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { ActivityIndicator } from 'react-native';
 import { StyleSheet, Text, View } from 'react-native';
+import moment from 'moment';
+import { Card, Button } from 'react-native-elements';
+import { Icon } from 'react-native-elements';
+import { FlatList } from 'react-native';
+import { Linking } from 'react-native';
+
+// api: a67f848775c44b098e81d0f243cd3dc1
+
+const filterForUniqueArticles = arr => {
+  const cleaned = [];
+  arr.forEach(itm => {
+    let unique = true;
+    cleaned.forEach(itm2 => {
+      const isEqual = JSON.stringify(itm) === JSON.stringify(itm2);
+      if (isEqual) unique = false;
+    });
+    if (unique) cleaned.push(itm);
+  });
+  return cleaned;
+};
 
 export default function App() {
+  const [loading, setLoading] = useState(true);
+  const [articles, setArticles] = useState([]);
+  const [pageNumber, setPageNumber] = useState(1);
+  const [hasErrored, setHasApiError] = useState(false);
+  const [lastPageReached, setLastPageReached] = useState(false);
+  
+  const getNews = async () => {
+    if (lastPageReached) return;
+    setLoading(true);
+    try {
+      const hasMoreArticles = jsonData.articles.length > 0;
+      const response = await fetch(
+        `https://newsapi.org/v2/top-headlines?country=us&apiKey=a67f848775c44b098e81d0f243cd3dc1&page=${pageNumber}`
+      );
+      const jsonData = await response.json();
+      if (hasMoreArticles) {
+      const newArticleList = filterForUniqueArticles(
+        articles.concat(jsonData.articles)
+      );
+      setArticles(newArticleList);
+      setPageNumber(pageNumber + 1);
+      } else {
+        setLastPageReached(true);
+      }
+    } catch (error) {
+      setHasApiError(false);
+    }
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    getNews();
+  }, [articles]);
+
+  const renderArticleItem = ({ item }) => {
+    return (
+      <Card title={item.title} image={{ uri: item.urlToImage }}>
+        <View style={styles.row}>
+          <Text style={styles.label}>Source</Text>
+          <Text style={styles.info}>{item.source.name}</Text>
+        </View>
+        <Text style={{ marginBottom: 10 }}>{item.content}</Text>
+        <View style={styles.row}>
+          <Text style={styles.label}>Published</Text>
+          <Text style={styles.info}>
+            {moment(item.publishedAt).format('LLL')}
+          </Text>
+        </View>
+        <Button icon={<Icon />} title="Read more" backgroundColor="#03A9F4" onPress={() => onPress(item.url)} />
+      </Card>
+    );
+  };
+
+  const onPress = url => {
+    Linking.canOpenURL(url).then(supported => {
+      if (supported) {
+        Linking.openURL(url);
+      } else {
+        console.log(`Don't know how to open URL: ${url}`);
+      }
+    });
+  };
+
+  if (loading) {
+    return (
+      <View style={styles.container}>
+        <ActivityIndicator
+          size="large"
+          loading={loading}
+        />
+      </View>
+    )
+  }
+
+  if (hasErrored) {
+    return (
+      <View style={styles.container}>
+        <Text>Error =(</Text>
+      </View>
+    )
+  }
+  
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.row}>
+        <Text style={styles.label}>Articles Count:</Text>
+        <Text style={styles.info}>{articles.length}</Text>
+      </View>
+      <FlatList
+        data={articles}
+        onEndReached={getNews}
+        onEndReachedThreshold={1}
+        renderItem={renderArticleItem}
+        keyExtractor={item => item.title}
+        ListFooterComponent={lastPageReached ? <Text>No more articles</Text> : <ActivityIndicator
+          size="large"
+          loading={loading}
+        />}
+      />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  containerFlex: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    marginTop: 40,
     alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#fff',
+    justifyContent: 'center'
   },
+  header: {
+    height: 30,
+    width: '100%',
+    backgroundColor: 'pink'
+  },
+  row: {
+    flexDirection: 'row'
+  },
+  label: {
+    fontSize: 16,
+    color: 'black',
+    marginRight: 10,
+    fontWeight: 'bold'
+  },
+  info: {
+    fontSize: 16,
+    color: 'grey'
+  }
 });
